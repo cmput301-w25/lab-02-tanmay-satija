52074cefcc91e8c44a5ee7b387d92caecb7e339d
tanmay-satija <157076642+tanmay-satija@users.noreply.github.com>
2025-01-17 16:39:43 -0700

    code

---
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayList<String> dataList;
+    ArrayAdapter<String> cityAdapter;
+    EditText cityNameInput;
+    Button addCityButton, deleteCityButton;
+    int selectedPosition = -1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        cityList = findViewById(R.id.city_list);
+        cityNameInput = findViewById(R.id.city_name_input);
+        addCityButton = findViewById(R.id.add_city_button);
+        deleteCityButton = findViewById(R.id.delete_city_button);
+
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney","Berlin"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter  = new ArrayAdapter<>(this , R.layout.content , dataList);
+        cityList.setAdapter(cityAdapter);
+
+        Button confirmButton = findViewById(R.id.confirm_button);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newCity = cityNameInput.getText().toString();
+                if (!newCity.isEmpty()) {
+                    dataList.add(newCity);
+                    cityAdapter.notifyDataSetChanged();
+                    cityNameInput.setText("");
+                    Toast.makeText(MainActivity.this, "City added", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter city name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newCity = cityNameInput.getText().toString();
+                if (!newCity.isEmpty()) {
+                    dataList.add(newCity);
+                    cityAdapter.notifyDataSetChanged();
+                    cityNameInput.setText("");
+                    Toast.makeText(MainActivity.this, "City added", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter city name", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition = position;
+                Toast.makeText(MainActivity.this, "Selected: " + dataList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        deleteCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPosition != -1) {
+                    dataList.remove(selectedPosition);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedPosition = -1;
+                    Toast.makeText(MainActivity.this, "City deleted", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Please select city to delete", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+
+    }
+}
